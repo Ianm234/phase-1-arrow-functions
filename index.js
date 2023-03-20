36e2178c4b5bc96ad671f20b5c923aff7cb9fcac
Ianm234 <126569965+Ianm234@users.noreply.github.com>
2023-03-20 17:28:43 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function divide(number1,number2){
+    console.log (number1/number2)
+    }
+    divide(2000,100)
+const square = (x) => x*x
+   square(2)
+   const add = (a,b) => a+b
+   add (3,4)
+  
